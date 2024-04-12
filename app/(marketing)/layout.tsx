3ecdbb4d6a996a614d4ce636d4e6c7fb25dd5163
kgbinh <binhkhongg@gmail.com>
2024-04-12 15:52:44 +0700

    authentication (login, signin) with clerk

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,18 +1,18 @@
-import Logo from '@/components/logo'
-import React from 'react'
-import { NavBar } from './_components/navBar'
-import { Footer } from './_components/footer'
+import Logo from '@/components/logo';
+import React from 'react';
+import { NavBar } from './_components/navBar';
+import { Footer } from './_components/footer';
 
 export default function MarketingLayout({
     children,
 }: {
-    children: React.ReactNode
+    children: React.ReactNode;
 }) {
     return (
         <div className="h-full bg-slate-100">
             <NavBar />
-            <main className="pb-2 pt-40">{children}</main>
+            <main className="pb-20 pt-40">{children}</main>
             <Footer />
         </div>
-    )
+    );
 }
