55849a14805d9439e7f6c01fe45d34b790f39299
kgbinh <binhkhongg@gmail.com>
2024-04-15 21:34:50 +0700

    xử lí server action (tạo board, xóa board)

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -4,18 +4,41 @@ import { z } from 'zod';
 
 import { db } from '@/lib/db';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+export type State = {
+    errors?: {
+        title?: string[];
+    };
+    message?: null | string;
+};
 
 const CreateBoard = z.object({
-    title: z.string(),
+    title: z.string().min(3, {
+        message: 'Minimum length of 3 letters is required',
+    }),
 });
-export const create = async (formData: FormData) => {
-    const { title } = CreateBoard.parse({
+export const create = async (prevState: State, formData: FormData) => {
+    const validateFields = CreateBoard.safeParse({
         title: formData.get('title'),
     });
-    await db.board.create({
-        data: {
-            title,
-        },
-    });
+    if (!validateFields.success) {
+        return {
+            errors: validateFields.error.flatten().fieldErrors,
+            message: 'Missing fields.',
+        };
+    }
+    const { title } = validateFields.data;
+    try {
+        await db.board.create({
+            data: {
+                title,
+            },
+        });
+    } catch (error) {
+        return {
+            message: 'DB error',
+        };
+    }
     revalidatePath('organization/org_2f1wiGFaN8plUh8dghAISTCqBn3');
+    //redirect('organization/org_2f1wiGFaN8plUh8dghAISTCqBn3');
 };
